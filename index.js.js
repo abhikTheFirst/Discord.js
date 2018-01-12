43ff6cb8d9fbac2612da1bbf5723a7509b8dbda6
Abhik Kumar <arc.dawn007@gmail.com>
2018-01-12 08:28:02 -0600

    files

---
+++ index.js.js
@@ -0,0 +1,10 @@
+console.log('Hello world');
+// theres no such thing as integers.
+console.log('josh is not british');
+
+for(let x = 0; x <= 7; x++)
+{
+    console.log('josh is stupid.');
+    if(x === 5)
+        console.log('josh is special.');
+}
